81f7157877713d666f2afd757cc7a60f125a1dbc
M.Saad <jahangirarslan321>
2026-02-03 03:36:35 -0800

    Remove navbar collapse to prevent interference with Link navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,11 +15,8 @@ function NavBar() {
           <span className="logo-emoji">🌾</span>
           <span className="brand-text">Farmer Aid</span>
         </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
-          <span className="navbar-toggler-icon"></span>
-        </button>
 
-        <div className="collapse navbar-collapse" id="mainNav">
+        <div className="navbar-collapse">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
             <li className="nav-item"><Link className="nav-link" to="/weather">Weather</Link></li>
